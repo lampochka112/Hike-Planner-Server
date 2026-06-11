3f7b6e2677abfd62d9a25dc5a4679a8ff70015cb
Jesse <daniilgolosov232@gmail.com>
2026-06-11 23:22:44 +0300

    Create create-users.js

---
+++ src/migrations/create-users.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        unique: true
+      },
+      password_hash: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      first_name: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      avatar_url: {
+        type: Sequelize.STRING(500),
+        allowNull: true
+      },
+      experience_level: {
+        type: Sequelize.ENUM('новичок', 'любитель', 'опытный', 'профессионал'),
+        defaultValue: 'новичок'
+      },
+      bio: {
+        type: Sequelize.TEXT,
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('users');
+  }
+};
